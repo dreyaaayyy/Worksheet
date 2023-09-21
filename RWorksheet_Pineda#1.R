95d885cb6f70fa8a54d21294e9c23716fadadd4e
Andrea Patrice Pineda <andreapatrice.pineda@students.isatu.edu.ph>
2023-09-21 04:03:01 +0000

    updated commit

--- RWorksheet_Pineda#1.R
+++ RWorksheet_Pineda#1.R
@@ -33,3 +33,42 @@ age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19,
   newVdata <- Vdata * 2
   newVdata
   
+#8 sequence
+#8.1
+  int1to100 <- seq(1,100)
+  int1to100
+
+#8.2
+  num20to60 <-seq(20,60)
+  num20to60
+  
+#8.3
+  meanofnum20to60 <-mean(num20to60)
+  meanofnum20to60
+  
+#8.4
+  seq51to91 <-seq(51,91)
+  sumofnum51to91 <- sum(seq51to91)
+  sumofnum51to91
+  
+#8.5
+  int1to1000 <- seq(1,1000)
+  int1to1000
+  
+#8.a
+  allDataPoints <-length(int1to100) + length(num20to60) + length(meanofnum20to60) + length(sumofnum51to91)
+  allDataPoints
+  
+#8.c
+  maxdatapoints <-seq(1,10)
+  maxuntil10 <-max(maxdatapoints)
+  maxuntil10
+  
+#9
+  
+#10 backward sequence
+  int1to100 <- seq(100,1)
+  int1to100
+
+#11 numbers below 25 multiples of 3 or 5
+  
