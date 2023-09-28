966029df89329c5a7269ef1a1c23ef54558a9cd6
Andrea Patrice Pineda <andreapatrice.pineda@students.isatu.edu.ph>
2023-09-28 03:53:37 +0000

    initial commit

---
+++ RWorksheet_Pineda#2.R
@@ -0,0 +1,34 @@
+# Operator
+#1a.
+  Operator <- seq (-5,5)
+  Operator
+  
+  #The output is a sequence from -5 to 5
+  
+#1.b
+  x <- 1:7
+  x
+  
+  #The value of x is a sequence from 1 up to 7
+  
+#2
+  seq(1,3, by=0.2)
+
+#3.a
+  WorkerAge <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+           35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 
+           50, 37, 46, 25, 17, 37, 43, 53, 41, 51,
+           35, 24, 33, 41, 53, 40, 18, 44, 38, 41, 
+           48, 27, 39, 19, 30, 61, 54, 58, 26, 18)
+  WorkerAge
+  
+  element3rd <- WorkerAge [3]
+  element3rd
+
+#3.b
+  element2nd <- WorkerAge [2]
+  element2nd
+  element4th <- WorkerAge [4]
+  element4th
+
+#3.c
